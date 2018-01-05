ebab95459b36615d1ad008510ab6395bed25ef6e
kommer <braber@gmail.com>
2018-01-05 14:19:42 +0100

    Initial spring boot app

---
+++ src/main/java/nl/braber/binance/BinanceApplication.java
@@ -0,0 +1,12 @@
+package nl.braber.binance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceApplication.class, args);
+	}
+}
